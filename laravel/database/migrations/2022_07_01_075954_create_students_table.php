7a6d99b0bdcbbe51a191a75371661bd702046a10
Bùi Quỳnh Ninh <ninhbq15052002@gmail.com>
2022-07-02 14:22:26 +0700

    update

---
+++ laravel/database/migrations/2022_07_01_075954_create_students_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            //tạo các cột trong db
+            $table->string('name');
+            $table->string('address')->nullable(); //có thể ko điền địa chỉ
+            // Thêm 1 dòng mới vào db 
+            // -> sử dụng lệnh php artisan migrate:rollback để xóa trắng dữ liệu
+            // Sử dụng lại lệnh php artisan migrate để lưu dữ liệu mới
+            $table->date('time_start'); 
+            $table->integer('status')->default(1); //đặt giá trị mặc định cho status là 1
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +22,42 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // Tạo dữ liệu test
+        // for ($i=0; $i <100 ; $i++) { 
+            
+        // }
+        $dataStudentSeed = [
+            [
+                'name'=>'BUI QUYNH NINH',
+                'address'=>'', // có thể bỏ trống vì đã dùng nullable
+                'time_start'=> '2022-07-01',
+                'created_at'=> date('Y-m-d H:i:s'),
+                'updated_at'=> date('Y-m-d H:i:s')
+            ],
+            // [
+            //     'name'=>'BUI QUYNH NINH',
+            //     'address'=>'', // có thể bỏ trống vì đã dùng nullable
+            //     'time_start'=> '2022-07-02',
+            //     'created_at'=> date('Y-m-d H:i:s'),
+            //     'updated_at'=> date('Y-m-d H:i:s')
+            // ],
+        ];
+
+        // Insert vào bảng Students
+
+        $dataLoop = [];
+        for ($i=1; $i <= 100 ; $i++) { 
+            // $dataStudentSeed['name'].$i;
+            array_push($dataStudentSeed, [
+                'name'=>'BUI QUYNH NINH' .$i,
+                'address'=>'', // có thể bỏ trống vì đã dùng nullable
+                'time_start'=> '2022-07-01',
+                'created_at'=> date('Y-m-d H:i:s'),
+                'updated_at'=> date('Y-m-d H:i:s')
+            ],);
+        }
+        DB::table('students')->insert($dataStudentSeed);
+
     }
 }
